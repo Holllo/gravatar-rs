471656abc58575deaf818fc4eef03656705e5c0a
Bauke <me@bauke.xyz>
2022-04-02 22:04:17 +0200

    Replace individual snapshots with a Vec snapshot.

--- tests/lib.rs
+++ tests/lib.rs
@@ -6,23 +6,23 @@ const HOLLLO_EMAIL: &str = "helllo@holllo.cc";
 #[test]
 fn test_hash_email() {
   let samples = [("bauke", BAUKE_EMAIL), ("holllo", HOLLLO_EMAIL)];
+  let mut snapshot = vec![];
 
   for (name, email) in samples {
-    insta::assert_snapshot!(
-      format!("hash-{name}"),
-      Generator::hash_email(email)
-    );
+    snapshot.push((format!("hash-{name}"), Generator::hash_email(email)));
 
-    insta::assert_snapshot!(
+    snapshot.push((
       format!("hash-{name}-whitespace"),
-      Generator::hash_email(&format!("  {email}  "))
-    );
+      Generator::hash_email(&format!("  {email}  ")),
+    ));
 
-    insta::assert_snapshot!(
+    snapshot.push((
       format!("hash-{name}-casing"),
-      Generator::hash_email(&email.to_uppercase())
-    );
+      Generator::hash_email(&email.to_uppercase()),
+    ));
   }
+
+  insta::assert_debug_snapshot!("hash-email", snapshot);
 }
 
 #[test]
@@ -32,12 +32,15 @@ fn test_generator() {
     ("gravatar", Generator::default().base_url),
     ("libravatar", "cdn.libravatar.org".to_string()),
   ];
+  let mut snapshot = vec![];
 
   for (name, base_url) in samples {
     let generator = Generator::default().set_base_url(&base_url);
     let urls = emails.map(|email| generator.generate(email));
-    insta::assert_debug_snapshot!(format!("generate-{name}"), urls);
+    snapshot.push((format!("generate-{name}"), urls));
   }
+
+  insta::assert_debug_snapshot!("generator", snapshot);
 }
 
 #[test]
